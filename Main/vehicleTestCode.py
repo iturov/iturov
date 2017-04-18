b439e09ba92b39ec2db7871485d69a42cc02104b
VatanTezer <vatanaksoytezer@gmail.com>
2017-04-18 20:15:00 +0000

    Updated Code

--- Main/vehicleTestCode.py
+++ Main/vehicleTestCode.py
@@ -71,9 +71,9 @@ def motors_write():
         ######-- Z AXIS ESC CONTROL --######
         #print dataArray
 
-	dataArrayInt = [int(dataArray[0]),int(dataArray[1]),int(dataArray[2]),int(dataArray[3]),0,0]
-	servoDriver.set_servo_pulsewidth(lightDriver, 1200 + dataArrayInt[3]*7)
-	print dataArrayInt[3]
+	dataArrayInt = [int(dataArray[0]),int(dataArray[1]),int(dataArray[2]),int(dataArray[3]),0,0,0]
+	servoDriver.set_servo_pulsewidth(lightDriver, 1200 + dataArrayInt[3])
+	print str(dataArrayInt[3]) + "hey"
 	if(dataArrayInt[0] > 10):
 		dataArrayInt[0] += 500
 	if(dataArrayInt[0] < -10):
