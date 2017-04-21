28fc7297f1cb40ec95f1d25346d3d837500c944d
VatanTezer <vatanaksoytezer@gmail.com>
2017-04-21 20:06:44 +0000

    Organized test

--- Main/sensors/sensor_test.py
+++ Main/sensors/sensor_test.py
@@ -6,16 +6,8 @@ import time
 
 def main():
     sensor = Sensor(19, 26)
-    #sensor.debug()
     while True:
-        sensor.read_jst()
-        sensor.read_pressure()
-        dist = sensor.distance
-        temperature = sensor.temperature
-        pressure = sensor.pressure_mb
-        depth = sensor.freshwater_depth
-        print("Distance: %0.1f cm\t Temp: %0.2f C\tPressure:  %0.1f mbar") % (dist, temperature, pressure)
-
+        sensor.debug()
 
 if __name__ == "__main__":
     main()
