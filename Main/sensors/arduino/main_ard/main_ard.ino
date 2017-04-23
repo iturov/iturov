31cc14bba935981008db7bc12b77b20d7bef09c0
Vatan <vatanaksoytezer@gmail.com>
2017-04-23 14:05:48 +0300

    Fixed Arduino Codes

--- Main/sensors/arduino/main_ard/main_ard.ino
+++ Main/sensors/arduino/main_ard/main_ard.ino
@@ -1,12 +1,17 @@
 #include <Wire.h>
-
+#include <SoftwareSerial.h>
 #include "TSYS01.h"
 
+SoftwareSerial BTSerial(2, 3); // RX | TX
 TSYS01 sensor;
+String bt_data = String('0');
+
 
 void setup() {
   
   Serial.begin(115200);
+
+  BTSerial.begin(9600);  
     
   Wire.begin();
 
@@ -15,10 +20,16 @@ void setup() {
 }
 
 void loop() {
-
-  sensor.read();
   
-  Serial.println(sensor.temperature()); 
-  
+  if (BTSerial.available()) 
+  {
+    Serial.write(BTSerial.read());
+  }
+  else 
+  {
+    sensor.read();
+    String temp(sensor.temperature());
+    Serial.println(temp);
+  }
   delay(100);
 }
