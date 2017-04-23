88bfe84b637b56771ecf600510ffd7b1e7158ecd
VatanTezer <vatanaksoytezer@gmail.com>
2017-04-23 14:16:30 +0000

    Fixed Distance Sensor Code

--- Main/sensors/read_jst.py
+++ Main/sensors/read_jst.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
         while True:
             dist = distance()
             print ("Measured Distance = %.1f cm" % dist)
-            time.sleep(1)
+            time.sleep(0.2)
  
         # Reset by pressing CTRL + C
     except KeyboardInterrupt:
