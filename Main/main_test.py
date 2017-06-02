c7e6a9f6e0e953589c1c5cd9f0cb9e4af65983af
senceryazici <sencer_yazici98@hotmail.com>
2017-06-02 19:52:44 +0300

    Updating Modules!

--- Main/main_test.py
+++ Main/main_test.py
@@ -10,7 +10,7 @@ from modules.motorControllers import controller
 _network = network
 _motor_controller = controller
 motor_pins = [17,18,27,25,16,21]
-servo_off_value = 1500
+servo_off_value = 1450
 incoming_data = []
 
 
@@ -24,13 +24,13 @@ if True:
     thread.start_new_thread(_variable_control,())
     _network.initialize('192.168.137.1')
     _network.establish()
-    
+
 #    time.sleep(1)
     _motor_controller.initialize(motor_pins,servo_off_value)
 #    time.sleep(1)
     _motor_controller.run()
     print "Run"
- 
+
     try:
         while 1:
             continue
