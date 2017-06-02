79960672995b70fa45098bb868e8b0730143de23
Unknown <sencer_yazici98@hotmail.com>
2017-06-03 02:34:03 +0300

    Updatin controller

--- Main/modules/motorControllers/controller.py
+++ Main/modules/motorControllers/controller.py
@@ -1,8 +1,18 @@
+# NOTE: BeeROV motor controller algoritm/structure. functions starting with "_" underscore are private functions that user does NOT call.
+
 import pigpio
 import time
 import thread
+import pid
 
+# NOTE: PID's should be enabled, see function "run"
+depth_pid = pid
+roll_pid = pid
+
+# NOTE: in this algoritm a reversible motor library is created, uses 1500 us as an off VALUE
 servo_off_value = 1500
+depth_feedback = 0
+roll_feedback
 servo_driver = pigpio.pi()
 arrayInt = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 pins = [0,0,0,0,0,0]
@@ -39,12 +49,43 @@ def _run_thread():
         #conversion
         for i in range(0,len(pins)):
             motors[i] = _constrain(motors[i], -500, 500)
-            servo_driver.set_servo_pulsewidth(pins[i],servo_off_value + motors[i])
+            servo_driver.set_servo_pulsewidth(pins[i], servo_off_value + motors[i])
         print motors
         time.sleep(0.02)
 
-def run():
-    thread.start_new_thread(_run_thread,())
+def run(pid_state = 0): # NOTE: set pid_state to 1 to enable pid controller
+    if pid_state == 0:
+        thread.start_new_thread(_run_thread,()) # NOTE: start without a pid controller
+    if pid_state == 1:
+        thread.start_new_thread(_run_with_pid,()) # NOTE: start with a pid controller
+
+def _run_with_pid():
+    motors = [0,0,0,0,0,0]
+    while 1:
+        global arrayInt
+        global pins
+        #conversion
+        z_speed = arrayInt[0]
+        fowardBack = arrayInt[1]
+        leftRight = arrayInt[2]
+        roll_speed = arrayInt[7]
+        yaw = arrayInt[8]
+        throttle = depth_pid.calculate(z_speed, depth_feedback, 1, 1, 1, 1) # NOTE: feedback values are coming from sensor depth sensor
+        roll = roll_pid.calculate(roll_speed, roll_feedback, 1, 1, 1, 1) # NOTE: feedback2 value comes from the IMU sensor
+        motors[0] = throttle + roll # NOTE: multiply roll with -1 if correction is wrong
+        motors[1] = throttle - roll
+        # XY-plane
+        motors[2] = (-fowardBack) + (-leftRight) + (-yaw) # motor3
+        motors[3] = (-fowardBack) + (leftRight) + (yaw) # motor4
+        motors[4] = (fowardBack) + (-leftRight) + (yaw) # motor5
+        motors[5] = (fowardBack) + (leftRight) + (-yaw) # motor6
+        #conversion
+        # NOTE: the rest of the motors on the XY plane are non-pid controlled motors, will be added in the future
+        for i in range(2,len(pins)):
+            motors[i] = _constrain(motors[i], -500, 500)
+            servo_driver.set_servo_pulsewidth(pins[i], servo_off_value + motors[i])
+        print motors
+        time.sleep(0.02)
 
 def _constrain(value, min = 0, max = 1000):
     if value > max:
--- Main/modules/motorControllers/pid.py
+++ Main/modules/motorControllers/pid.py
@@ -1,30 +1,12 @@
-#IMPORT LIBRARIES
-#...
+# storing Integral, and the previous error to calculate rate of change in the error
+I = 0
+error_prev = 0
 
-#CREATE CLASS
-class PID(object):
-
-    def __init__(self):
-        self.P = 0 # CREATE PROPORTIONAL VALUE
-        self.I = 0 # CREATE INTEGRAL VALUE
-        self.D = 0 # CREATE DERIVATIVE VALUE
-        self.Eprev = 0 # CREATE PREVIOUS ERROR VALUE
-
-        # INITIALIZE OBJECT
-        #...
-
-    def calculate(self,setPoint,feedBack, Kp, Ki, Kd, dt):
-        self.setPoint = setPoint
-        self.feedBack = feedBack
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
-        self.dt = dt
-
-        self.error = self.setPoint - self.feedBack
-        self.P = self.error * self.Kp # CALCULATE P TERM
-        self.I = self.I + self.error * self.Ki * self.dt # CALCULATE I TERM
-        self.D = (self.error - self.Eprev) * self.Kd / self.dt # CALCULATE D TERM
-        self.Eprev = self.error # UPDATE PREVIOUS ERROR BY NEW ERROR
-        # NOTE: "dt" IS THE LOOP TIME
-        return(self.P + self.I + self.D) # RETURN THE CALCULATED VALUE
+def calculate(setpoint, feedback, kp, ki, kd, dt):
+    error = setpoint - feedback
+    P = error * kp
+    I += error * ki * dt
+    D = (error - error_prev) * kd / dt
+    error_prev = error
+    # NOTE: DT IS LOOP TIME
+    return (P + I + D)
