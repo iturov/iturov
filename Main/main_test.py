fffe38d82bc4301f5a2ac92bae460277e5d7ad6b
senceryazici <sencer_yazici98@hotmail.com>
2017-06-02 17:22:42 +0300

    Modules updated

--- Main/main_test.py
+++ Main/main_test.py
@@ -3,25 +3,25 @@ import thread
 sys.path.append("/modules/motorController")
 sys.path.append("/modules/network")
 
-import controller as motor_controller
+import controller as _motor_controller
 import network as _network
 
 motor_pins = []
-servo_off_value = 0
+servo_off_value = 1500
 incoming_data = []
 
 
-def variable_control():
+def _variable_control():
     while True:
         incoming_data = _network.dataArray
-        motor_controller.arrayInt = incoming_data
+        _motor_controller.arrayInt = incoming_data
 
 if True:
-    thread.start_new_thread(variable_control,())
+    thread.start_new_thread(_variable_control,())
     _network.initialize()
     _network.establish()
-    motor_controller.initialize(motor_pins,servo_off_value)
-    motor_controller.run()
+    _motor_controller.initialize(motor_pins,servo_off_value)
+    _motor_controller.run()
 
 
     try:
